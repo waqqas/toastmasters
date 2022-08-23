2e092b5d10187c3dd662dc8397581664df8cfcc3
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2022-08-23 07:46:53 +0000

    Added gamification app

--- toastmasters/urls.py
+++ toastmasters/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "gamification/",
+        include(("gamification.urls", "gamification"), namespace="gamification"),
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
