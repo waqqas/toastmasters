0549d6c0d3b2ede558e404198c0772503dadff9b
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2022-08-23 08:26:15 +0000

    Show users in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """User model."""
+    """User model"""
