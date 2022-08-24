2437173bcd4f631e0baff46494518b2c460f3453
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2022-08-24 14:20:56 +0000

    Added basic event models

--- event/admin.py
+++ event/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .admin_models import EventAdmin
-from .models import Event
+from .admin_models import EventAdmin, ParticipationAdmin, PerformedRoleAdmin, RoleAdmin
+from .models import Event, Participation, PerformedRole, Role
 
 admin.site.register(Event, EventAdmin)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(Participation, ParticipationAdmin)
+admin.site.register(PerformedRole, PerformedRoleAdmin)
--- gamification/models.py
+++ gamification/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from event.models import Role
+
 
 class Point(models.Model):
-    role = models.CharField(max_length=64, default="", unique=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
     points = models.IntegerField(default=0)
 
     def __str__(self):
