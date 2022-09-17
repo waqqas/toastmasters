65a2048cff6bc783ad270c68e745e120e5715ccc
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2022-09-17 18:53:06 +0000

    Handling if domain is valid but not registered

--- pknic/serializers.py
+++ pknic/serializers.py
@@ -8,7 +8,7 @@ from rest_framework import serializers
 
 class LookupSerializer(serializers.Serializer):
     domain = serializers.CharField(required=True, max_length=50)
-    registered = serializers.BooleanField(required=True)
+    registered = serializers.BooleanField(required=False)
     created = serializers.DateField(required=False)
     expiry = serializers.DateField(required=False)
     name_servers = serializers.ListField(
@@ -16,11 +16,8 @@ class LookupSerializer(serializers.Serializer):
     )
 
 
-class LookupErrorSerializer(serializers.Serializer):
-    pass
-
-
 class LookupPageParserState(str, enum.Enum):
+    START = "START"
     DOMAIN_NAME_KEY = "DOMAIN_NAME_KEY"
     DOMAIN_NAME_VALUE = "DOMAIN_NAME_VALUE"
     DOMAIN_REG_STATUS = "DOMAIN_REG_STATUS"
@@ -31,54 +28,74 @@ class LookupPageParserState(str, enum.Enum):
     DNS_NAMESERVERS_KEY = "DNS_NAMESERVERS_KEY"
     DNS_NAMESERVERS_VALUE = "DNS_NAMESERVERS_VALUE"
     COMPLETE = "COMPLETE"
+    DOMAIN_NOT_FOUND_KEY = "DOMAIN_NOT_FOUND_KEY"
+    DOMAIN_NOT_FOUND_VALUE = "DOMAIN_NOT_FOUND"
 
 
 class DomainSerializer(serializers.Serializer):
     domain = serializers.CharField(required=True, max_length=50)
 
     def _get_page_data(self, page) -> dict:
-        state: LookupPageParserState = LookupPageParserState.DOMAIN_NAME_KEY
+        state: LookupPageParserState = LookupPageParserState.START
         response = {}
 
         table = page.find("table", border="0", width="707")
-        for data in table.stripped_strings:
-            if state == LookupPageParserState.DOMAIN_NAME_KEY and data == "Domain Name":
-                state = LookupPageParserState.DOMAIN_NAME_VALUE
-            elif state == LookupPageParserState.DOMAIN_NAME_VALUE:
-                response["domain"] = data
-                state = LookupPageParserState.DOMAIN_REG_STATUS
-            elif state == LookupPageParserState.DOMAIN_REG_STATUS:
-                if data == "Domain is Registered":
-                    response["registered"] = True
-                else:
-                    response["registered"] = False
-                state = LookupPageParserState.CREATE_DATE_KEY
-            elif (
-                state == LookupPageParserState.CREATE_DATE_KEY
-                and data == "Create Date:"
-            ):
-                state = LookupPageParserState.CREATE_DATE_VALUE
-            elif state == LookupPageParserState.CREATE_DATE_VALUE:
-                response["created"] = parse(data).date()
-                state = LookupPageParserState.EXPIRY_DATE_KEY
-            elif (
-                state == LookupPageParserState.EXPIRY_DATE_KEY
-                and data == "Expire Date:"
-            ):
-                state = LookupPageParserState.EXPIRY_DATE_VALUE
-            elif state == LookupPageParserState.EXPIRY_DATE_VALUE:
-                response["expiry"] = parse(data).date()
-                state = LookupPageParserState.DNS_NAMESERVERS_KEY
-            elif (
-                state == LookupPageParserState.DNS_NAMESERVERS_KEY
-                and data == "DNS Nameservers"
-            ):
-                response["name_servers"] = []
-                state = state = LookupPageParserState.DNS_NAMESERVERS_VALUE
-            elif state == LookupPageParserState.DNS_NAMESERVERS_VALUE:
-                if data != "None Specified (or deleted)":
-                    response["name_servers"].append(data)
-                else:
+        if table:
+            state = LookupPageParserState.DOMAIN_NAME_KEY
+            for data in table.stripped_strings:
+                if (
+                    state == LookupPageParserState.DOMAIN_NAME_KEY
+                    and data == "Domain Name"
+                ):
+                    state = LookupPageParserState.DOMAIN_NAME_VALUE
+                elif state == LookupPageParserState.DOMAIN_NAME_VALUE:
+                    response["domain"] = data
+                    state = LookupPageParserState.DOMAIN_REG_STATUS
+                elif state == LookupPageParserState.DOMAIN_REG_STATUS:
+                    if data == "Domain is Registered":
+                        response["registered"] = True
+                    else:
+                        response["registered"] = False
+                    state = LookupPageParserState.CREATE_DATE_KEY
+                elif (
+                    state == LookupPageParserState.CREATE_DATE_KEY
+                    and data == "Create Date:"
+                ):
+                    state = LookupPageParserState.CREATE_DATE_VALUE
+                elif state == LookupPageParserState.CREATE_DATE_VALUE:
+                    response["created"] = parse(data).date()
+                    state = LookupPageParserState.EXPIRY_DATE_KEY
+                elif (
+                    state == LookupPageParserState.EXPIRY_DATE_KEY
+                    and data == "Expire Date:"
+                ):
+                    state = LookupPageParserState.EXPIRY_DATE_VALUE
+                elif state == LookupPageParserState.EXPIRY_DATE_VALUE:
+                    response["expiry"] = parse(data).date()
+                    state = LookupPageParserState.DNS_NAMESERVERS_KEY
+                elif (
+                    state == LookupPageParserState.DNS_NAMESERVERS_KEY
+                    and data == "DNS Nameservers"
+                ):
+                    response["name_servers"] = []
+                    state = state = LookupPageParserState.DNS_NAMESERVERS_VALUE
+                elif state == LookupPageParserState.DNS_NAMESERVERS_VALUE:
+                    if data != "None Specified (or deleted)":
+                        response["name_servers"].append(data)
+                    else:
+                        state = LookupPageParserState.COMPLETE
+        else:
+            response["registered"] = False
+
+            state = LookupPageParserState.DOMAIN_NOT_FOUND_KEY
+            for data in page.stripped_strings:
+                if (
+                    state == LookupPageParserState.DOMAIN_NOT_FOUND_KEY
+                    and data == "Domain not found:"
+                ):
+                    state = LookupPageParserState.DOMAIN_NOT_FOUND_VALUE
+                elif state == LookupPageParserState.DOMAIN_NOT_FOUND_VALUE:
+                    response["domain"] = data
                     state = LookupPageParserState.COMPLETE
 
         return response
